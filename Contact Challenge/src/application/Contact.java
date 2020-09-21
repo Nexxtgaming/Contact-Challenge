2fa37669cef3be3f5d53ec9cd74a65eee968db56
maciekd12 <maciek.pilot2@gmail.com>
2020-09-21 17:11:30 +0200

    Adding a project to GitHub

---
+++ Contact Challenge/src/application/Contact.java
@@ -0,0 +1,32 @@
+package application;
+
+public class Contact {
+	private String firstName,lastName, notes ;
+	private String phoneNumber;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	};
+	
+	
+}
